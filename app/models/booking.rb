cccf93fe3060be399b1bb1340f4005841a948e73
Yann Deret <yann.deretleberre@gmail.com>
2022-09-15 15:33:00 +0200

    missions ok

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :listing
+  def create!
+    create!
+    Mission.create!(listing_id: self.listing_id, mission_type: 'first_checkin', date: self.start_date, price: 20)
+    Mission.create!(listing_id: self.listing_id, mission_type: 'last_checkout', date: self.end_date, price: 5)
+  end
 end
